548c7e97b0aa18292199bb4b529f37fce2c364e7
Alan Marble <alanrmarble@gmail.com>
2018-07-24 20:54:14 -0700

    Initial API endpoints

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const ObjectID = mongodb.ObjectID;
+
+const USERS_COLLECTION = 'users';
+
+const app = express();
+app.use(bodyParser.json());
+
+let db;
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, (error, client) => {
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
+
+  db = client.db();
+  console.log('Database connection successful');
+
+  const server = app.listen(process.env.PORT || 8080, () => {
+    const port = server.address().port;
+    console.log('Application running on port', port);
+  });
+});
+
+/**
+ * Generic error handler
+ * @param res
+ * @param reason
+ * @param message
+ * @param code
+ */
+function handleError(res, reason, message, code) {
+  console.log('ERROR: ' + reason);
+  res.status(code || 500).json({'error': message});
+}
+
+// Authentication routes
+
+app.get('/api/token/:login/:password', (req, res) => {
+  db.collection(USERS_COLLECTION).findOne({login: req.params.login}, (err, doc) => {
+    if (err) {
+      handleError(res, err.message, 'Login or password is incorrect');
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
