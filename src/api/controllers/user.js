c58e2a85915a32d7fbfd725595301439f4adf871
Alan Marble <alanrmarble@gmail.com>
2018-07-25 19:13:57 -0700

    Fix user.post

--- src/api/controllers/user.js
+++ src/api/controllers/user.js
@@ -1,16 +1,14 @@
 import User from '../models/user';
 
 exports.post = (req, res) => {
-  User.findOne({ email: req.body.email }, (err, user) => {
-    const data = Object.assign({}, req.body, {user: req.user.sub}) || {};
+  const data = Object.assign({}, req.body, {user: req.user.sub}) || {};
 
-    User.create(data, (err, user) => {
-      if (err || !user) {
-        res.status(500).json(err);
-      } else {
-        user.password = undefined;
-        res.status(200).json(user);
-      }
-    });
+  User.create(data, (err, user) => {
+    if (err || !user) {
+      res.status(500).json(err);
+    } else {
+      user.password = undefined;
+      res.status(200).json(user);
+    }
   });
 };
