f6d4dadc7fc12422d41cb7558a27eaae8abc2b1f
Alan Marble <alanrmarble@gmail.com>
2018-07-26 20:36:04 -0700

    More cors stuff

--- src/api/server.js
+++ src/api/server.js
@@ -14,7 +14,7 @@ api.use(jwt({ secret: process.env.JWT_SECRET })).unless({
 });
 
 api.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
