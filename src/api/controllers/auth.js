80e174a1e1ba07b5ef8b94ff41a26f88e98e0ccf
Alan Marble <alanrmarble@gmail.com>
2018-07-27 19:28:29 -0700

    users

--- src/api/controllers/auth.js
+++ src/api/controllers/auth.js
@@ -4,11 +4,11 @@ import jwt from 'jsonwebtoken';
 exports.post = (req, res) => {
   User.findOne({ email: req.body.email }, (err, user) => {
     if (err || !user) {
-      res.status(401).json({error: 'Email or password incorrect'});
+      res.status(401).json({error: 'Email or password incorrect', user: user});
     } else {
       user.verifyPassword(req.body.password, (bErr, result) => {
         if (bErr || !result) {
-          res.status(401).json({error: 'Email or password incorrect'});
+          res.status(401).json({error: 'Email or password incorrect', user2: user});
         } else {
           const token = jwt.sign(
             { id: user._id, userLevel: user.userLevel },
